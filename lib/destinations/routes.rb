8dc1e71b2ed60ffcee4094aeb5f5dd82fb9971ae
Tobias Crawley <tcrawley@gmail.com>
2011-03-10 15:03:38 -0500

    Remove /topic/message rouutes, since we don't support message viewing on topics.

--- lib/destinations/routes.rb
+++ lib/destinations/routes.rb
@@ -1,21 +1,18 @@
 
 module Backstage
   class Application < Sinatra::Base
-    %w{ queue topic }.each do |model|
-      resource model, :view_path => :destinations, :actions => [:pause, :resume]
-      
-      get "/#{model}/:name/messages" do
-        @destination = eval( model.classify ).find( Util.decode_name( params[:name] ) )
-        haml :'messages/index'
-      end
+    resource :queue, :topic, :view_path => :destinations, :actions => [:pause, :resume]
 
-      get "/#{model}/:name/message/:id" do
-        @destination = eval( model.classify ).find( Util.decode_name( params[:name] ) )
-        @object = @destination.find { |m| m.jms_id == Util.decode_name( params[:id] ) }
-        haml :'messages/show'
-      end
+    get "/queue/:name/messages" do
+      @destination = Queue.find( Util.decode_name( params[:name] ) )
+      haml :'messages/index'
+    end
 
+    get "/queue/:name/message/:id" do
+      @destination = Queue.find( Util.decode_name( params[:name] ) )
+      @object = @destination.find { |m| m.jms_id == Util.decode_name( params[:id] ) }
+      haml :'messages/show'
     end
   end
 end
-    
+
